9828b8868a490a3530d5179d8cf9433c6afa8c61
Raghadaladwan <raghadaledwan97@gmail.com>
2019-11-12 23:15:13 +0200

    First Page

--- Server/server.js
+++ Server/server.js
@@ -4,38 +4,44 @@ const db = require("./db");
 
 const app = express();
 app.use(cors());
-app.use((request, response, next) => {
-  response.header(`Access-Control-Allow-Origin`, `*`);
-  response.header(
-    `Access-Control-Allow-Headers`,
-    `Origin, X-Requested-with, Content-Type, Accept`
-  );
-  next();
-});
+//__________________USELESS CODE ACTUALLY________________
+
+// app.use((request, response, next) => {
+//   response.header(`Access-Control-Allow-Origin`, `*`);
+//   response.header(
+//     `Access-Control-Allow-Headers`,
+//     `Origin, X-Requested-with, Content-Type, Accept`
+//   );
+//   next();
+// });
+
+//_______________________________________________________
+
 app.use(express.json());
 
 app.get("/", (req, res) => res.json("test working"));
 
 //SERVER AND DATABASE RESPONSE AND BEHAVIOR
 
-//Please write your code below and only below here 
+//Please write your code below and only below here
 //Start here
 
+app.post("/register", (req, res) => {
+  db.register(response => {
+    res.send(response);
+  });
+});
 
+//________________________________________GET BEST TRIPS
 
-
-
-
-
-
-
-
-
-
-
+app.get("/getBestTrips", (req, res) => {
+  db.getBestTrips(response => {
+    res.json(response);
+  }, req.body);
+});
 
 //End here
-//Port 
+//Port
 const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
